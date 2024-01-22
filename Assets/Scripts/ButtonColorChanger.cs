45f4d9e2d6eea88d90b91d822b0052f528824ada
Alan Joy <2460256J@student.gla.ac.uk>
2024-01-22 18:32:21 +0000

    check mark appears when you select image

--- Assets/Scripts/ButtonColorChanger.cs
+++ Assets/Scripts/ButtonColorChanger.cs
@@ -3,37 +3,27 @@ using UnityEngine.UI;
 
 public class ButtonColorChanger : MonoBehaviour
 {
-    public Button myButton; // Reference to the Button component
-
-    void Start()
-    {
-        // // Check if the button reference is assigned
-        // if (myButton != null)
-        // {
-        //     // Change the button's color to red
-        //     ChangeButtonColor(Color.red);
-        // }
-        // else
-        // {
-        //     Debug.LogError("Button reference not assigned. Please assign the button in the Unity Editor.");
-        // }
-    }
+    private Button myButton;
+    private bool selected = false;
 
     // Function to change the color of the button
-    void ChangeButtonColor(Color newColor)
+    public void ChangeButtonColor()
     {
-        // Get the button's image component
-        Image buttonImage = myButton.GetComponent<Image>();
-
-        // Check if the button has an image component
-        if (buttonImage != null)
+        myButton = GetComponent<Button>();
+        Image tickImage = myButton.transform.Find("tick").GetComponent<Image>();
+        float newAlpha;
+        if (selected == false)
         {
-            // Update the color of the button's image
-            buttonImage.color = newColor;
+            newAlpha = 1f;
+            selected = true;
         }
         else
         {
-            Debug.LogError("Button does not have an Image component. Please make sure the button has an Image component to change its color.");
+            newAlpha = 0f;
+            selected = false;
         }
+        Color currentColor = tickImage.color;
+        Color newColor = new Color(currentColor.r, currentColor.g, currentColor.b, newAlpha);
+        tickImage.color = newColor;
     }
 }
